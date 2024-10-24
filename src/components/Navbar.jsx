369b63aa2d6bf07d21a23480865a36cd73947f51
Matheus D. Santosr <matheus1819.santos20@gmail.com>
2024-10-24 17:49:51 -0300

    fix: the sintax of the ul tag in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,8 +35,8 @@ const Navbar = () => {
               key={link.id}
               className={`${active === link.title
                   ? "text-white"
-                  : "text-secondary"
-                }hover:text-white text-[18px] font-medium cursor-pointer`}
+                  : "text-secondary"}
+                  hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>{link.title}</a>
