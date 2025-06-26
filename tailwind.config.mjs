08a21de29dc8846f8c929c59c81b93071c037e78
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-06-26 17:48:20 -0300

    chore: change primary-light color and add secondary-light color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,8 +10,9 @@ module.exports = {
     extend: {
       colors: {
         primary: "#050816",
-        "primary-light": "#CFFFF6",
+        "primary-light": "#E6E6FA",
         secondary: "#aaa6c3",
+        "secondary-light": "#702963",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
