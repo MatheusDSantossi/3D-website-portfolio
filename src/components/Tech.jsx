804dbdea6d94f599dd7ed42fb30184aeac9fc458
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-02 20:59:17 -0300

    feat(tech): add the new component `ProgressBar` on `Tech` and update the tech experience percentage

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,17 +1,21 @@
-import { BallCanvas } from './canvas';
-import { SectionWrapper } from '../hoc';
-import { technologies } from '../constants';
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import ProgressBar from "./ui/ProgressBar";
 
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+        <div key={technology.name} className="flex w-72 gap-2 items-center">
+          <div className="w-28 h-28">
+            <BallCanvas icon={technology.icon} />
+          </div>
+          <ProgressBar value={technology.experience} />
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Tech, "")
+export default SectionWrapper(Tech, "");
--- src/constants/index.js
+++ src/constants/index.js
@@ -76,58 +76,72 @@ import {
     {
       name: "HTML 5",
       icon: html,
+      experience: "95%"
     },
     {
-      name: "CSS 3",
+      name: "CSS",
       icon: css,
+      experience: "95%"
     },
     {
       name: "JavaScript",
       icon: javascript,
+      experience: "95%"
     },
     {
       name: "TypeScript",
       icon: typescript,
+      experience: "95%"
     },
     {
-      name: "React JS",
+      name: "React",
       icon: reactjs,
+      experience: "95%"
     },
     {
       name: "Java",
       icon: java,
+      experience: "90%"
     },
     {
       name: "Tailwind CSS",
       icon: tailwind,
+      experience: "95%"
     },
     {
       name: "Node JS",
       icon: nodejs,
+      experience: "85%"
     },
     {
       name: "PostgreSQL",
       icon: postgresql,
+      experience: "85%"
     },
     {
       name: "Python",
       icon: python,
+      experience: "95%"
     },
     {
       name: "git",
       icon: git,
+      experience: "85%"
     },
     {
       name: "figma",
       icon: figma,
+      experience: "90%"
     },
     {
       name: "docker",
       icon: docker,
+      experience: "75%"
     },
     {
       name: "MySQL",
       icon: mysql,
+      experience: "85%"
     },
   ];
   
